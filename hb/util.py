29ca7959c65d81fae4723055e35ab98d8077cdbb
y-chan <y_chan_crypto@yahoo.co.jp>
2020-10-29 01:33:38 +0900

    add load and dump function

--- hb/util.py
+++ hb/util.py
@@ -1,3 +1,8 @@
+from typing import Dict, List
+
+import json
+
+
 def int_to_bytes(num: int) -> bytes:
     if num < 0xfd:
         return num.to_bytes(1, "little")
@@ -19,3 +24,19 @@ def bits_to_target(bits: int) -> int:
     if not (0x8000 <= bitsBase <= 0x7fffff):
         raise Exception("Second part of bits should be in [0x8000, 0x7fffff]")
     return bitsBase << (8 * (bitsN-3))
+
+
+def load_and_dump_json(file_name: str, content: Dict) -> None:
+    with open(f"../blockchain_data/{file_name}") as f:
+        file: List[Dict] = json.loads(f.read())
+    file.append(content)
+    with open(f"../blockchain_data/{file_name}", "w") as f:
+        f.write(json.dumps(file))
+
+
+def dump_block(block: Dict) -> None:
+    load_and_dump_json("blockchain", block)
+
+
+def dump_tx(tx: Dict) -> None:
+    load_and_dump_json("tx", tx)
